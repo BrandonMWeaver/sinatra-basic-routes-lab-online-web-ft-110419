7a6428e2e2a745ed38d66a250f8e9049af78ef25
Brandon Weaver <a7xncob@gmail.com>
2019-12-21 21:07:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get "/name" { return "Brandon Weaver" }
-  get "/hometown" { return "Leesburg, Virginia" }
-  get "/favorite-song" { return "In The Fire" }
+  get("/name") { return "Brandon Weaver" }
+  get("/hometown") { return "Leesburg, Virginia" }
+  get("/favorite-song") { return "In The Fire" }
   
 end
